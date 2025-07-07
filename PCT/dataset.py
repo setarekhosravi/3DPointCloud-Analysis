c163faf2a6332d07dfe4efde9b8e17ff13366673
setarekhosravi <strhkhosravi@gmail.com>
2025-07-07 15:04:22 +0330

    change the order of function and classes

--- PCT/dataset.py
+++ PCT/dataset.py
@@ -34,60 +34,6 @@ import plotly.express as px
 path = Path("Datasets/ModelNet10")   
 # print(path)
 
-# read .off files contains vertices and faces
-def read_off(file):
-    if 'OFF' != file.readline().strip():
-        raise('Not a valid OFF header')
-    n_verts, n_faces, _ = tuple([int(item) for item in file.readline().strip().split(' ')])
-    verts = [[float(s) for s in file.readline().strip().split(' ')] for i_vert in range(n_verts)]
-    faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for i_face in range(n_faces)]
-    return verts, faces
-
-# visualize the mesh
-def visualize_rotate(data):
-    x_eye, y_eye, z_eye = 1.25, 1.25, 0.8
-    frames=[]
-
-    def rotate_y(x, y, z, theta):
-        w = x+1j*y
-        return np.real(np.exp(1j*theta)*w), np.imag(np.exp(1j*theta)*w), z
-    
-    for t in np.arange(0, 10.26, 0.1):
-        xe, ye, ze = rotate_y(x_eye, y_eye, z_eye, -t)
-        frames.append(dict(layout=dict(scene=dict(camera=dict(eye=dict(x=xe, y=ye, z=ze))))))
-    
-    fig = go.Figure(data=data,
-                    layout=go.Layout(
-                        updatemenus=[dict(type='buttons',
-                                          showactive=False,
-                                          y=1,
-                                          x=0.8,
-                                          xanchor='left',
-                                          yanchor='bottom',
-                                          pad=dict(t=45, r=10),
-                                          buttons=[dict(label='Play',
-                                                        method='animate',
-                                                        args=[None, dict(frame=dict(duration=50, redraw=True),
-                                                                         transition=dict(duration=0),
-                                                                         fromcurrent=True,
-                                                                         mode='immediate')]
-                                                        )]
-                                            )]
-                                    ), 
-                                    frames=frames
-                    )
-    return fig
-
-# visualize the point cloud
-def pcshow(xs,ys,zs):
-    data = go.Figure(data=[go.Scatter3d(x=xs, y=ys, z=zs,
-                                        mode='markers')])
-    fig = visualize_rotate(data)
-    fig.update_traces(marker=dict(size=2,
-                                  line=dict(color='darkblue', width=2)), 
-                                  selector=dict(mode='markers'))
-    fig.show()
-
 # we can use open3d to sample from the mesh to extract a point cloud
 class PointSampler(object):
     def __init__(self, output_size):
@@ -156,6 +102,60 @@ class RandomNoise(object):
         noise = np.random.normal(0, 0.02, (pointcloud.shape))
         noisy_pointcloud = pointcloud + noise
         return noisy_pointcloud
+    
+# read .off files contains vertices and faces
+def read_off(file):
+    if 'OFF' != file.readline().strip():
+        raise('Not a valid OFF header')
+    n_verts, n_faces, _ = tuple([int(item) for item in file.readline().strip().split(' ')])
+    verts = [[float(s) for s in file.readline().strip().split(' ')] for i_vert in range(n_verts)]
+    faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for i_face in range(n_faces)]
+    return verts, faces
+
+# visualize the mesh
+def visualize_rotate(data):
+    x_eye, y_eye, z_eye = 1.25, 1.25, 0.8
+    frames=[]
+
+    def rotate_y(x, y, z, theta):
+        w = x+1j*y
+        return np.real(np.exp(1j*theta)*w), np.imag(np.exp(1j*theta)*w), z
+    
+    for t in np.arange(0, 10.26, 0.1):
+        xe, ye, ze = rotate_y(x_eye, y_eye, z_eye, -t)
+        frames.append(dict(layout=dict(scene=dict(camera=dict(eye=dict(x=xe, y=ye, z=ze))))))
+    
+    fig = go.Figure(data=data,
+                    layout=go.Layout(
+                        updatemenus=[dict(type='buttons',
+                                          showactive=False,
+                                          y=1,
+                                          x=0.8,
+                                          xanchor='left',
+                                          yanchor='bottom',
+                                          pad=dict(t=45, r=10),
+                                          buttons=[dict(label='Play',
+                                                        method='animate',
+                                                        args=[None, dict(frame=dict(duration=50, redraw=True),
+                                                                         transition=dict(duration=0),
+                                                                         fromcurrent=True,
+                                                                         mode='immediate')]
+                                                        )]
+                                            )]
+                                    ), 
+                                    frames=frames
+                    )
+    return fig
+
+# visualize the point cloud
+def pcshow(xs,ys,zs):
+    data = go.Figure(data=[go.Scatter3d(x=xs, y=ys, z=zs,
+                                        mode='markers')])
+    fig = visualize_rotate(data)
+    fig.update_traces(marker=dict(size=2,
+                                  line=dict(color='darkblue', width=2)), 
+                                  selector=dict(mode='markers'))
+    fig.show()
 
 if __name__ == "__main__":
     with open(path/"chair/train/chair_0001.off", "r") as f:
