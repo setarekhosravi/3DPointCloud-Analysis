1ac3899d9d132fb2d87005d03564587a19c3efed
setarekhosravi <strhkhosravi@gmail.com>
2025-07-09 17:46:57 +0330

    add the network of pointnet

---
+++ PointNet/model.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    This code contains Point Transformer Networks.
+    Created on Wed Jul 9 13:27 2025
+    @author: STRH
+    From 3D Data Science Book.
+"""
+
+# import libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# T-net class from pointnet paper
+class Tnet(nn.Module):
+    def __init__(self, k=3):
+        super().__init__()
+
+        # contains of shared mlp
+        self.k=k
+        self.conv1 = nn.Conv1d(k,64,1)
+        self.conv2 = nn.Conv1d(64,128,1)
+        self.conv3 = nn.Conv1d(128,1024,1)
+        self.fc1 = nn.Linear(1024,512)
+        self.fc2 = nn.Linear(512,256)
+        self.fc3 = nn.Linear(256,k*k)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+    def forward(self, x):
+        # input shape [bs,n,3]
+        bs = x.size(0)
+        xb = F.relu(self.bn1(self.conv1(x)))
+        xb = F.relu(self.bn2(self.conv2(xb)))
+        xb = F.relu(self.bn3(self.conv3(xb)))
+
+        pool = nn.MaxPool1d(xb.size(-1))(xb)
+        flat = nn.Flatten(1)(pool)
+
+        xb = F.relu(self.bn4(self.fc1(flat)))
+        xb = F.relu(self.bn5(self.fc2(xb)))
+
+        # initialize as identity
+        init = torch.eye(self.k, requires_grad=True).repeat(bs,1,1)
+        if xb.is_cuda:
+            init=init.cuda()
+        matrix = self.fc3(xb).view(-1,self.k,self.k) + init
+        return matrix
+    
+# input transformation and feature transformation
+class Transform(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.input_transform = Tnet(k=3)
+        self.feature_transform = Tnet(k=64)
+        
+        self.conv1 = nn.Conv1d(3,64,1)
+        self.conv2 = nn.Conv1d(64,128,1)
+        self.conv3 = nn.Conv1d(128,1024,1)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+
+    def forward(self, x):
+        matrix3x3 = self.input_transform(x)
+        # batch matrix multiplication
+        xb = torch.bmm(torch.transpose(x,1,2), matrix3x3).transpose(1,2)
+        xb = F.relu(self.bn1(self.conv1(xb)))
+        matrix64x64 = self.feature_transform(xb)
+        xb = torch.bmm(torch.transpose(xb,1,2), matrix64x64).transpose(1,2)
+        xb = F.relu(self.bn2(self.conv2(xb)))
+        xb = self.bn3(self.conv3(xb))
+        xb = nn.MaxPool1d(xb.size(-1))(xb)
+        output = nn.Flatten(1)(xb)
+        return output, matrix3x3, matrix64x64
+    
+# finally the pointnet network
+class PointNet(nn.Module):
+    def __init__(self, classes = 10):
+        super().__init__()
+        self.transform = Transform()
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512,256)
+        self.fc3 = nn.Linear(256, classes)
+
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.dropout = nn.Dropout(p=0.3)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def foraward(self, x):
+        xb, matrix3x3, matrix64x64 = self.transform(x)
+        xb = F.relu(self.bn1(self.fc1(xb)))
+        xb = F.relu(self.bn2(self.dropout(self.fc2(xb))))
+        xb = self.fc3(xb)
+        output = self.logsoftmax(xb)
+        return output, matrix3x3, matrix64x64
